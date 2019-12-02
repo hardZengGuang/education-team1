357e6163efa5523324135db57e6edcd4b3e2e1fd
zengguang <1521405768@qq.com>
2019-12-02 15:28:42 +0800

    教育管理系统

---
+++ roncoo-education/roncoo-education-server-eureka/src/main/java/com/roncoo/education/server/eureka/ServerEurekaApplication.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2015-现在 广州市领课网络科技有限公司
+ */
+package com.roncoo.education.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ * 
+ * @author wujing
+ */
+@EnableEurekaServer
+//@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEurekaApplication.class, args);
+        System.out.println("注册中心(server-eureka)启动成功，请接着启动配置中心(server-config)");
+    }
+    
+}
